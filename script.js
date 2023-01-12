1f8bed46902225ed6d05e18ca09edf2b645ee7a6
Jim Jorgensen <jim@uptrust.co>
2023-01-12 12:30:41 -0500

    added the script to fetch and save 3 randomly generated people from the api and variable net worths

--- script.js
+++ script.js
@@ -14,5 +14,15 @@ async function getRandomUser() {
     const res = await fetch('https://randomuser.me/api')
     const data = await res.json();
   
-    const user = data.esults[0]
+    const user = data.results[0]
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random()* 1000000)
+    }
+    addData(newUser)
+}
+
+function addData(obj) {
+    data.push(obj)
 }
