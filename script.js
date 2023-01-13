e65cbe79a5084605ee1ad4670dc9ba54f07fd6da
Jim Jorgensen <jim@uptrust.co>
2023-01-12 19:04:51 -0500

    finished calculating entire wealth and adding it to the DOM

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ async function getRandomUser() {
 
     const newUser = {
         name: `${user.name.first} ${user.name.last}`,
-        money: Math.floor(Math.random()* 1000000)
+        money: Math.floor(Math.random()* 2000000)
     }
     addData(newUser)
 }
@@ -33,6 +33,23 @@ function sortByRichest() {
     data.sort((a, b) => b.money - a.money);
     updateDOM()
 }
+//only shows the millionares on the list
+function showMillionaires() {
+  data = data.filter((item) => {
+    return item.money > 1000000;
+  });
+  updateDOM();
+}
+
+//calculates all the wealth from all the users
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0)
+   
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth:<strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl)
+}
+
 function addData(obj) {
     data.push(obj)
 
@@ -62,3 +79,5 @@ function formatMoney(number) {
 addUserBtn.addEventListener('click', getRandomUser)
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
